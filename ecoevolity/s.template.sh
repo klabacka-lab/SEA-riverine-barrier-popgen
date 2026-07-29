c81dc1c60aeead0207be79a64c3a3463d8ae2b07
Randy Klabacka <rklaback@login04.rc.byu.edu>
2026-07-28 22:53:58 -0600

    ecoevolity completed

---
+++ ecoevolity/s.template.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=10000M   # memory per CPU core
+#SBATCH -J "Trofimets"   # job name
+#SBATCH --mail-user=rklaback@byu.edu   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+module load miniforge3/25.3.1-0
+eval "$(conda shell.bash hook)"
+source ~/myTools/miniconda3/etc/profile.d/conda.sh
+conda activate phylo_align
+
+cd ~/Projects/SEA-riverine-barrier-popgen/ecoevolity/<river_name>/runs
+ecoevolity --relax-triallelic-sites --relax-missing-sites config.yml
